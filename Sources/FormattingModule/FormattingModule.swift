3f220b929ffab3b037c271aa93a8340341f79dc7
Tariel Khalvashi <taro.khalvashi1225@gmail.com>
2024-11-11 16:12:40 +0400

    FormattingModule

--- Sources/FormattingModule/FormattingModule.swift
+++ Sources/FormattingModule/FormattingModule.swift
@@ -1,2 +1,37 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public protocol FormattingProtocol {
+    func formatDate(_ dateString: String) -> String
+}
+
+public class DateFormatterForNews: FormattingProtocol {
+    private let dateFormatter: DateFormatter
+    
+    public init() {
+        self.dateFormatter = DateFormatter()
+    }
+    
+    public func formatDate(_ dateString: String) -> String {
+        let formats = [
+            "yyyy-MM-dd'T'HH:mm:ssZ",
+            "yyyy-MM-dd'T'HH:mm:ss.SSSZ",
+            "yyyy-MM-dd HH:mm:ss",
+            "yyyy-MM-dd"
+        ]
+        
+        for format in formats {
+            dateFormatter.dateFormat = format
+            if let date = dateFormatter.date(from: dateString) {
+                dateFormatter.dateFormat = "MMM dd, yyyy"
+                return dateFormatter.string(from: date)
+            }
+        }
+        
+        print("Failed to parse date: \(dateString)")
+        return "Unknown Date"
+    }
+}
+
